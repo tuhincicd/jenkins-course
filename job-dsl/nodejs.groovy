80ea89437ed246168ae467d0aa342acd8b59a70e
tuhincicd <chinsat51@gmail.com>
2019-06-14 13:58:48 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -2,7 +2,7 @@ job('NodeJS example') {
     scm {
         git('git://github.com/tuhincicd/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('chinsat51@gmail.com')
         }
     }
     triggers {
